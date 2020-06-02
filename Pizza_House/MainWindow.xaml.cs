f5a90eab0f4ba98be4f6d66f8d260cb8029e4de9
Nicklas Geisnaes <nicklasg1998@live.dk>
2020-06-02 11:53:47 +0200

    Base_Setup

---
+++ Pizza_House/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pizza_House
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetUp();
+        }
+
+        public void SetUp()
+        {
+            foreach (Canvas item in FindWindowChildren<Canvas>(Body))
+            {
+                if (item.Name.Split('_')[0].Contains("Details"))
+                    item.Visibility = Visibility.Visible;
+                else
+                    item.Visibility = Visibility.Hidden;
+            }
+        }
+
+
+
+        /// <summary>
+        /// Get controls of type
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="dObj"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> FindWindowChildren<T>(DependencyObject dObj) where T : DependencyObject
+        {
+            if (dObj != null)
+            {
+                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(dObj); i++)
+                {
+                    DependencyObject ch = VisualTreeHelper.GetChild(dObj, i);
+                    if (ch != null && ch is T)
+                    {
+                        yield return (T)ch;
+                    }
+
+                    foreach (T nestedChild in FindWindowChildren<T>(ch))
+                    {
+                        yield return nestedChild;
+                    }
+                }
+            }
+        }
+        private void Add_Btn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Change_Panel(object sender, MouseButtonEventArgs e)
+        {
+            foreach (Canvas item in FindWindowChildren<Canvas>(Body))
+            {
+                if (item.Name.Split('_')[0].Contains(((Image)sender).Tag.ToString()))
+                    item.Visibility = Visibility.Visible;
+                else
+                    item.Visibility = Visibility.Hidden;
+            }
+        }
+    }
+}
